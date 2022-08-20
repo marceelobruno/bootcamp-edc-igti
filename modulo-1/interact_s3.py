4ff519458010543ad88b3507b8879dcf645a0eea
Marcelo Bruno <mbrunorp@gmail.com>
2022-08-20 14:12:44 -0300

    modulo-1

---
+++ modulo-1/interact_s3.py
@@ -0,0 +1,26 @@
+import boto3
+import pandas as pd
+
+# Script para testar conexão boto3 com AWS S3 que retorna todos os nomes dos buckets existentes
+# s3 = boto3.resource('s3')
+
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+
+# Criar um cliente que interage com AWS S3
+s3_client = boto3.client('s3')
+
+# Download de arquivo usando boto3
+s3_client.download_file('datalake-mbruno5-igti-edc',
+                        'data/dimensao_mesorregioes_mg-1.csv',
+                        'dimensao_mesorregioes_mg-1.csv')
+
+# Lendo o arquivo baixado com a lib pandas
+df = pd.read_csv("dimensao_mesorregioes_mg-1.csv", sep=";")
+print(df)
+
+# Upload de arquivo para AWS S3 bucket usando boto3
+s3_client.upload_file(r'C:\Users\sobm9\Mbruno\bootcamps\bootcamp-edc-igti\modulo-1\mockaroo_funcionario.csv',
+                      'datalake-mbruno5-igti-edc',
+                      'data/mockaroo_funcionario.csv')
